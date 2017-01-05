49db3433a6cf4de862a96006071d8fd2c827795c
Mantas Kaveckas <mantas.kaveckas@gmail.com>
2017-01-05 21:23:31 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import ReactSiema from './ReactSiema';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    state = {
+        slides: [
+            <img src="https://unsplash.it/600/350?image=10" alt="slide" />,
+            <img src="https://unsplash.it/600/350?image=11" alt="slide" />,
+            <img src="https://unsplash.it/600/350?image=12" alt="slide" />,
+            <img src="https://unsplash.it/600/350?image=13" alt="slide" />,
+            <img src="https://unsplash.it/600/350?image=14" alt="slide" />,
+            <img src="https://unsplash.it/600/350?image=15" alt="slide" />,
+            <img src="https://unsplash.it/600/350?image=16" alt="slide" />,
+        ]
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>React Siema</h2>
+                </div>
+                <div className="App-intro">
+                    <h4>
+                        Lightweight and simple carousel wrapper for React based on awesome <a href="https://pawelgrzybek.com/siema/">Siema</a>.
+                    </h4>
+                    <div className="App-intro__slider">
+                        <ReactSiema ref={(siema) => this.siema = siema}>
+                            {this.state.slides.map((slide, index) => <div key={index}>{slide}</div>)}
+                        </ReactSiema>
+                        <button onClick={() => this.siema.prev()}>Prev</button>
+                        <button onClick={() => this.siema.next()}>Next</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/reactSiema.js
+++ src/reactSiema.js
@@ -1,53 +1,263 @@
-import React, { Component, PropTypes } from 'react';
-import Siema from 'siema';
+import React, { Component } from 'react';
 
-export default class ReactSiema extends Component {
+const transformProperty = (() => {
+    const { transform } = document.documentElement.style;
+    if (typeof transform === 'string') {
+        return 'transform';
+    }
+    return 'WebkitTransform';
+})();
+
+class ReactSiema extends Component {
+    events = [
+        'onTouchStart', 'onTouchEnd', 'onTouchMove', 'onMouseDown', 'onMouseUp', 'onMouseLeave', 'onMouseMove'
+    ];
+
+    constructor(props) {
+        super();
+        this.config = Object.assign({}, {
+            duration: 200,
+            easing: 'ease-out',
+            perPage: 1,
+            startIndex: 0,
+            draggable: true,
+            threshold: 20,
+            loop: false,
+        }, props);
+
+        this.events.forEach((handler) => {
+            this[handler] = this[handler].bind(this);
+        });
+    }
+
     componentDidMount() {
-        this.siema = new Siema(Object.assign({}, this.props.options, {
-            selector: this.container
-        }));
+        this.config.selector = this.selector;
+        this.currentSlide = this.config.startIndex;
+
+        this.init();
+
+        this.onResize = () => {
+            this.resize();
+            this.slideToCurrent();
+        };
+
+        window.addEventListener('resize', this.onResize);
+
+        if (this.config.draggable) {
+            this.pointerDown = false;
+            this.drag = {
+                start: 0,
+                end: 0,
+            };
+        }
+    }
+
+    componentDidUpdate() {
+        this.init();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener(this.onResize);
+    }
+
+    init() {
+        this.setSelectorWidth();
+        this.setInnerElements();
+        this.resolveSlidesNumber();
+
+        this.setStyle(this.sliderFrame, {
+            width: `${(this.selectorWidth / this.perPage) * this.innerElements.length}px`,
+            webkitTransition: `all ${this.config.duration}ms ${this.config.easing}`,
+            transition: `all ${this.config.duration}ms ${this.config.easing}`
+        });
+
+        for (let i = 0; i < this.innerElements.length; i++) {
+            this.setStyle(this.innerElements[i], {
+                width: `${100 / this.innerElements.length}%`
+            });
+        }
+
+        this.slideToCurrent();
+    }
+
+    setSelectorWidth() {
+        this.selectorWidth = this.selector.getBoundingClientRect().width;
+    }
+
+    setInnerElements() {
+        this.innerElements = [].slice.call(this.sliderFrame.children);
+    }
+
+    resolveSlidesNumber() {
+        if (typeof this.config.perPage === 'number') {
+            this.perPage = this.config.perPage;
+        } else if (typeof this.config.perPage === 'object') {
+            this.perPage = 1;
+            for (const viewport in this.config.perPage) {
+                if (window.innerWidth > viewport) {
+                    this.perPage = this.config.perPage[viewport];
+                }
+            }
+        }
+    }
+
+    prev() {
+        if (this.currentSlide === 0 && this.config.loop) {
+            this.currentSlide = this.innerElements.length - this.perPage;
+        } else {
+            this.currentSlide = Math.max(this.currentSlide - 1, 0);
+        }
+        this.slideToCurrent();
+    }
+
+    next() {
+        if (this.currentSlide === this.innerElements.length - this.perPage && this.config.loop) {
+            this.currentSlide = 0;
+        } else {
+            this.currentSlide = Math.min(this.currentSlide + 1, this.innerElements.length - this.perPage);
+        }
+        this.slideToCurrent();
+    }
+
+    goTo(index) {
+        this.currentSlide = Math.min(Math.max(index, 0), this.innerElements.length - 1);
+        this.slideToCurrent();
+    }
+
+    slideToCurrent() {
+        this.sliderFrame.style[transformProperty] = `translate3d(-${this.currentSlide * (this.selectorWidth / this.perPage)}px, 0, 0)`;
+    }
+
+    updateAfterDrag() {
+        const movement = this.drag.end - this.drag.start;
+        if (movement > 0 && Math.abs(movement) > this.config.threshold) {
+            this.prev();
+        } else if (movement < 0 && Math.abs(movement) > this.config.threshold) {
+            this.next();
+        }
+        this.slideToCurrent();
+    }
+
+    resize() {
+        this.resolveSlidesNumber();
+
+        this.selectorWidth = this.selector.getBoundingClientRect().width;
+        this.setStyle(this.sliderFrame, {
+            width: `${(this.selectorWidth / this.perPage) * this.innerElements.length}px`
+        });
+    }
+
+    clearDrag() {
+        this.drag = {
+            start: 0,
+            end: 0,
+        };
+    }
+
+    setStyle(target, styles) {
+        Object.keys(styles).forEach((attribute) => {
+            target.style[attribute] = styles[attribute];
+        });
+    }
+
+    onTouchStart(e) {
+        e.stopPropagation();
+        this.pointerDown = true;
+        this.drag.start = e.touches[0].pageX;
+    }
+
+    onTouchEnd(e) {
+        e.stopPropagation();
+        this.pointerDown = false;
+        this.setStyle(this.sliderFrame, {
+            webkitTransition: `all ${this.config.duration}ms ${this.config.easing}`,
+            transition: `all ${this.config.duration}ms ${this.config.easing}`
+        });
+        if (this.drag.end) {
+            this.updateAfterDrag();
+        }
+        this.clearDrag();
+    }
+
+    onTouchMove(e) {
+        e.stopPropagation();
+        if (this.pointerDown) {
+            this.drag.end = e.touches[0].pageX;
+
+            this.setStyle(this.sliderFrame, {
+                webkitTransition: `all 0ms ${this.config.easing}`,
+                transition: `all 0ms ${this.config.easing}`,
+                [transformProperty]: `translate3d(${(this.currentSlide * (this.selectorWidth / this.perPage) + (this.drag.start - this.drag.end)) * -1}px, 0, 0)`
+            });
+        }
+    }
+
+    onMouseDown(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        this.pointerDown = true;
+        this.drag.start = e.pageX;
+    }
+
+    onMouseUp(e) {
+        e.stopPropagation();
+        this.pointerDown = false;
+        this.setStyle(this.sliderFrame, {
+            cursor: '-webkit-grab',
+            webkitTransition: `all ${this.config.duration}ms ${this.config.easing}`,
+            transition: `all ${this.config.duration}ms ${this.config.easing}`
+        });
+        if (this.drag.end) {
+            this.updateAfterDrag();
+        }
+        this.clearDrag();
+    }
+
+    onMouseMove(e) {
+        e.preventDefault();
+        if (this.pointerDown) {
+            this.drag.end = e.pageX;
+            this.setStyle(this.sliderFrame, {
+                cursor: '-webkit-grabbing',
+                webkitTransition: `all 0ms ${this.config.easing}`,
+                transition: `all 0ms ${this.config.easing}`,
+                [transformProperty]: `translate3d(${(this.currentSlide * (this.selectorWidth / this.perPage) + (this.drag.start - this.drag.end)) * -1}px, 0, 0)`
+            });
+        }
+    }
+
+    onMouseLeave(e) {
+        if (this.pointerDown) {
+            this.pointerDown = false;
+            this.drag.end = e.pageX;
+            this.setStyle(this.sliderFrame, {
+                cursor: '-webkit-grab',
+                webkitTransition: `all ${this.config.duration}ms ${this.config.easing}`,
+                transition: `all ${this.config.duration}ms ${this.config.easing}`
+            });
+            this.updateAfterDrag();
+            this.clearDrag();
+        }
     }
 
     render() {
         return (
             <div
-                className="siema"
-                ref={(container) => {
-                    this.container = container;
-                }}
+                ref={(selector) => this.selector = selector}
+                style={{ overflow: 'hidden' }}
+                {...this.events.reduce((props, event) => Object.assign({}, props, { [event]: this[event] }), {})}
             >
-                {this.props.children}
+                <div ref={(sliderFrame) => this.sliderFrame = sliderFrame}>
+                    {React.Children.map(this.props.children, (children, index) =>
+                        React.cloneElement(children, {
+                            key: index,
+                            style: { float: 'left' }
+                        })
+                    )}
+                </div>
             </div>
         );
     }
+}
 
-    next() {
-        this.siema.next();
-    }
-
-    prev() {
-        this.siema.prev();
-    }
-
-    goTo(index) {
-        this.siema.goTo(index);
-    }
-
-    getCurrentSlide() {
-        return this.siema.currentSlide;
-    }
-};
-
-ReactSiema.propTypes = {
-    duration: PropTypes.number,
-    easing: PropTypes.string,
-    perPage: PropTypes.number,
-    startIndex: PropTypes.number,
-    draggable: PropTypes.bool,
-    threshold: PropTypes.number,
-    loop: PropTypes.bool,
-    children: PropTypes.oneOfType([
-        React.PropTypes.element,
-        React.PropTypes.arrayOf(React.PropTypes.element)
-    ]).isRequired
-};
+export default ReactSiema;
