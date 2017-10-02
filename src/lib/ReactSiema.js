f535b2b8828c0c21d1ff29cba13100253b61c51c
Dennis Padiernos <depadiernos@gmail.com>
2017-10-02 18:51:49 -0500

    migrate to 'prop-types' package

--- src/lib/ReactSiema.js
+++ src/lib/ReactSiema.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import debounce from './utils/debounce';
 import transformProperty from './utils/transformProperty';
 
