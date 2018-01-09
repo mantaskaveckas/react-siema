f987884a6d4811979ece135e0c02756acd441889
Mark Mankarious <mark@neverbland.com>
2018-01-09 16:05:34 +0000

    Fix for bug that shows up when SSR is used (Razzle)

--- src/lib/utils/transformProperty.js
+++ src/lib/utils/transformProperty.js
@@ -1,4 +1,6 @@
 export default (() => {
+    if (typeof document === 'undefined') return;
+
     const { transform } = document.documentElement.style;
     if (typeof transform === 'string') {
         return 'transform';
